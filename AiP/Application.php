8748d84cadca42ed2bf60ebe878cb14b469d6d42
Henri Bergius <henri.bergius@iki.fi>
2011-07-10 00:08:59 +0300

    Bootstrap is loaded from aip.yaml

--- AiP/Application.php
+++ AiP/Application.php
@@ -1,7 +1,6 @@
 <?php
 namespace Midgard\AppServerBundle\AiP;
 
-require_once __DIR__.'/../../../../app/bootstrap.php.cache';
 require_once __DIR__.'/../../../../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
